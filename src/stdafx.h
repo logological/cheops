e100ded263314c93b46363fbe8bb57ce9210f746
Tristan Miller <psychonaut@nothingisreal.com>
2003-12-06 16:44:51 +0000

    Added GPL notices; removed Makefile

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,9 +1,28 @@
-// $Id: stdafx.h,v 1.2 2003-11-24 21:07:35 psy Exp $
+// $Id: stdafx.h,v 1.3 2003-12-06 16:44:51 psy Exp $
 //
 // stdafx.h : include file for standard system include files,
 //  or project specific include files that are used frequently, but
 //      are changed infrequently
 //
+//
+// Copyright (C) 2003 Tristan Miller <psychonaut@nothingisreal.com>
+//  
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+//
+//////////////////////////////////////////////////////////////////////
 
 #if !defined(AFX_STDAFX_H__912A39AC_33E4_11D3_BC7B_0080C84DB68D__INCLUDED_)
 #define AFX_STDAFX_H__912A39AC_33E4_11D3_BC7B_0080C84DB68D__INCLUDED_
